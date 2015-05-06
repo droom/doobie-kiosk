2e0dca39c7786238da9583da108c3c9ad0cab7a7
Chris Droom <chris.droom@gmail.com>
2015-05-06 21:17:33 +0800

    add switching, semantic ui

--- js/kiosk.js
+++ js/kiosk.js
@@ -7,33 +7,49 @@ $( document ).ready(function() {
     e.preventDefault();
   } );
 
+  // DRY THIS OUT!
 
   function backgroundReset(){
     $(".guide .column-1:nth-child(2)").css("background-color", bgOff);
-      $(".guide .column-1:nth-child(3)").css("background-color", bgOff);
-        $(".guide .column-1:nth-child(4)").css("background-color", bgOff);
-      }
+    $(".guide .column-1:nth-child(3)").css("background-color", bgOff);
+    $(".guide .column-1:nth-child(4)").css("background-color", bgOff);
+    $(".guide .column-1:nth-child(5)").css("background-color", bgOff);
+  };
 
-      $('.change-step-1').click(function() {
-        backgroundReset();
-        $(this).addClass('step-active');
-        $(".guide .column-1:nth-child(2)").css("background-color", bgOn);
-      });
-
-
-      $('.change-step-2').click(function() {
-        backgroundReset();
-        $(this).addClass('step-active');
-        $(".guide .column-1:nth-child(3)").css("background-color", bgOn);
-      });
-
-      $('.change-step-3').click(function() {
-        backgroundReset();
-        $(this).addClass('step-active');
-        $(".guide .column-1:nth-child(4)").css("background-color", bgOn);
-      });
+  $('.change-step-1').click(function() {
+    backgroundReset();
+    $(".guide .column-1:nth-child(2)").css("background-color", bgOn);
+  });
 
 
-    });
+  $('.change-step-2').click(function() {
+    backgroundReset();
+    $(".guide .column-1:nth-child(3)").css("background-color", bgOn);
+  });
+
+  $('.change-step-3').click(function() {
+    backgroundReset();
+    $(".guide .column-1:nth-child(4)").css("background-color", bgOn);
+    $(this).addClass('step-active');
+
+  });
+
+  $('.change-step-4').click(function() {
+    backgroundReset();
+    $(".guide .column-1:nth-child(5)").css("background-color", bgOn);
+    $(".checkout").addClass('show');
+
+  });
+
+  $('.checkout-close').click(function() {
+    $(".checkout").removeClass('show');
+  });
+
+
+
+
+
+
+});
 
 
